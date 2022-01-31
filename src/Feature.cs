38b51a772086e94d106fbb7ce014e9c65cccc53f
Tim Hanewich <tahanewich@live.com>
2022-01-30 21:04:46 -0500

    Added Feature class

---
+++ src/Feature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TimHanewich.NLP
+{
+    public class Feature
+    {
+        public int Offset {get; set;} //The beginning of the element
+        public int Length {get; set;} //How many characters long the element is
+
+        //Retrieves the content from the original source text provided
+        public string Text(string source)
+        {
+            if (source.Length < Offset)
+            {
+                throw new Exception("Unable to extract feature text: The source provided was not long enough.");
+            }
+
+            string ToReturn = source.Substring(Offset, Length);
+            return ToReturn;
+        }
+
+    }
+}
