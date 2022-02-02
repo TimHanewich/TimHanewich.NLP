5e5dc693ddd85176826e1a5e30d80d846fe8bc05
Tim Hanewich <tahanewich@live.com>
2022-02-02 11:20:40 -0500

    Fixed sentence reader

--- src/Feature.cs
+++ src/Feature.cs
@@ -22,8 +22,12 @@ namespace TimHanewich.NLP
 
         public string ReadContainingSentence(string source)
         {
-            int SentenceBegins = source.LastIndexOf(NlpToolkit.SentenceTerminators, Offset);
+            int SentenceBegins = source.LastIndexOf(NlpToolkit.SentenceTerminators, Offset); //An index of -1 is okay here, in  the case it does not find an end of a sentence previously. This is becaue the sentence extraction substring below moves the index up 1
             int SentenceEnds = source.IndexOf(NlpToolkit.SentenceTerminators, Offset + Length);
+            if (SentenceEnds == -1) //If we could not find the end of a sentence (for example, period with a space after), the end of the document itself must be the end of the sentence.
+            {
+                SentenceEnds = source.Length - 2;
+            }
             string ContainingSentence = source.Substring(SentenceBegins + 1, SentenceEnds - SentenceBegins + 1);
             ContainingSentence = ContainingSentence.Trim();
             return ContainingSentence;
