cf9ad53721f43330e54434377a8107f54a48e7d0
Tim Hanewich <tahanewich@live.com>
2022-02-02 10:31:21 -0500

    Added LastIndexof

--- src/NlpToolkit.cs
+++ src/NlpToolkit.cs
@@ -8,6 +8,9 @@ namespace TimHanewich.NLP
     public static class NlpToolkit
     {
 
+
+        #region "Index of"
+
         //Return the index of the first found value in the find parameters
         public static int IndexOf(this string src, string[] find, int start_at)
         {
@@ -32,6 +35,33 @@ namespace TimHanewich.NLP
             }
             return ToReturn;
         }
+
+        public static int LastIndexOf(this string src, string[] find, int start_at)
+        {
+            int ToReturn = -1;
+            foreach (string s in find)
+            {
+                int TryThis = src.LastIndexOf(s, start_at);
+                if (TryThis > -1)
+                {
+                    if (ToReturn == -1) //If ToReturn is the default (-1), just replace it with whatever this is. Because this is better than the default!
+                    {
+                        ToReturn = TryThis;
+                    }
+                    else //Since ToReturn already has a value (we found someting close earlier), we must test to see if this one is closer.
+                    {
+                        if (TryThis < ToReturn)
+                        {
+                            ToReturn = TryThis;
+                        }
+                    }
+                }
+            }
+            return ToReturn;
+        }
+
+        #endregion
+        
 
 
         public static string[] SeparateSentences(this string src)
