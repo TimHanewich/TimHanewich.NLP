6dc8dc2518cd2ad4c0f3a05762064bf816b5c2aa
Tim Hanewich <tahanewich@live.com>
2022-02-13 21:30:25 -0500

    Moved

--- src/Feature.cs
+++ src/Feature.cs
@@ -8,32 +8,6 @@ namespace TimHanewich.NLP
 
         public string Text {get; set;} //A copy of the text
 
-        //Retrieves the content from the original source text provided
-        public string Read(string source)
-        {
-            if (source.Length < Offset)
-            {
-                throw new Exception("Unable to extract feature text: The source provided was not long enough.");
-            }
-
-            string ToReturn = source.Substring(Offset, Length);
-            return ToReturn;
-        }
-
-        public string ReadContainingSentence(string source)
-        {
-            int SentenceBegins = source.LastIndexOf(NlpToolkit.SentenceTerminators, Offset); //An index of -1 is okay here, in  the case it does not find an end of a sentence previously. This is becaue the sentence extraction substring below moves the index up 1
-            int SentenceEnds = source.IndexOf(NlpToolkit.SentenceTerminators, Offset + Length);
-            if (SentenceEnds == -1) //If we could not find the end of a sentence (for example, period with a space after), the end of the document itself must be the end of the sentence.
-            {
-                SentenceEnds = source.Length - 2;
-            }
-            string ContainingSentence = source.Substring(SentenceBegins + 1, SentenceEnds - SentenceBegins + 1);
-            ContainingSentence = ContainingSentence.Trim();
-            return ContainingSentence;
-        }
-
-
         #region "Extraction Constructors"
 
         public static Feature[] IdentifyNumbers(string src)
